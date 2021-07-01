86652c493b5aa0871027930c762e351c10820430
Sergei Kolyago <Slellq@gmail.com>
2021-07-01 17:23:01 +0300

    update fileUpload

--- src/services/uploadAssets.ts
+++ src/services/uploadAssets.ts
@@ -1,20 +1,28 @@
 import sharp, { File } from 'sharp';
 import { constants, promises as fsPromise } from 'fs';
+import { createHash } from 'crypto';
+
+export function hashFromBuffer(buffer: Buffer, algorithm = 'sha256'): string {
+  const hash = createHash(algorithm);
+  hash.update(buffer);
+  return hash.digest('hex');
+}
 
 export default async function uploadAssets(file: File) {
   const buffer = await sharp(file.buffer)
     .resize({ weight: 300, height: 300 })
     .webp()
     .toBuffer();
-  const newPath = `${__dirname}/../../avatars/${file.originalname}`;
+  const hash = await hashFromBuffer(buffer);
+  const newPath = `${__dirname}/../../avatars/${hash}.webp`;
 
   try {
     await fsPromise.access(newPath, constants.F_OK);
-    throw new Error('file already exists');
+    Error('file already exists');
   } catch (e) {
     // ignore
   }
   await fsPromise.writeFile(newPath, buffer);
 
-  return `${process.env.BACKEND_URL}/static/${file.originalname}`;
+  return `${process.env.BACKEND_URL}/static/${hash}.webp`;
 }
